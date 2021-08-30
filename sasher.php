a64943281a0fc1bad87f94afa51905d18979de98
Hugh Barnes <dev@hubns.com>
2021-08-30 12:58:56 +1200

    Convert indents to tabs (grrr) and add composer instruction from devtools to autoloader

--- sasher.php
+++ sasher.php
@@ -10,50 +10,51 @@ use Grav\Common\Plugin;
  */
 class SasherPlugin extends Plugin
 {
-    /**
-     * @return array
-     *
-     * The getSubscribedEvents() gives the core a list of events
-     *     that the plugin wants to listen to. The key of each
-     *     array section is the event that the plugin listens to
-     *     and the value (in the form of an array) contains the
-     *     callable (or function) as well as the priority. The
-     *     higher the number the higher the priority.
-     */
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            'onPluginsInitialized' => [
-                // Uncomment following line when plugin requires Grav < 1.7
-                // ['autoload', 100000],
-                ['onPluginsInitialized', 0]
-            ]
-        ];
-    }
+	/**
+	 * @return array
+	 *
+	 * The getSubscribedEvents() gives the core a list of events
+	 *     that the plugin wants to listen to. The key of each
+	 *     array section is the event that the plugin listens to
+	 *     and the value (in the form of an array) contains the
+	 *     callable (or function) as well as the priority. The
+	 *     higher the number the higher the priority.
+	 */
+	public static function getSubscribedEvents(): array
+	{
+		return [
+			'onPluginsInitialized' => [
+				// Uncomment following line when plugin requires Grav < 1.7
+				// ['autoload', 100000],
+				['onPluginsInitialized', 0],
+			]
+		];
+	}
 
-    /**
-     * Composer autoload
-     *
-     * @return ClassLoader
-     */
-    public function autoload(): ClassLoader
-    {
-        return require __DIR__ . '/vendor/autoload.php';
-    }
+	/**
+	 * Composer autoload
+	 *
+	 * @return ClassLoader
+	 */
+	public function autoload(): ClassLoader
+	{
+		// run `cd /var/www/grav/user/plugins/sasher` and `composer update` to initialize the autoloader
+		return require __DIR__ . '/vendor/autoload.php';
+	}
 
-    /**
-     * Initialize the plugin
-     */
-    public function onPluginsInitialized(): void
-    {
-        // Don't proceed if we are in the admin plugin
-        if ($this->isAdmin()) {
-            return;
-        }
+	/**
+	 * Initialize the plugin
+	 */
+	public function onPluginsInitialized(): void
+	{
+		// Don't proceed if we are in the admin plugin
+		if ($this->isAdmin()) {
+			return;
+		}
 
-        // Enable the main events we are interested in
-        $this->enable([
-            // Put your main events here
-        ]);
-    }
+		// Enable the main events we are interested in
+		$this->enable([
+			// Put your main events here
+		]);
+	}
 }
